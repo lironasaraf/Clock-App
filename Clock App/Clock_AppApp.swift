2c20f1cd7bbd0b04293e9f21afb094ab9ce82f28
lironasaraf <lironi06@gmail.com>
2023-08-16 13:42:16 +0300

    Initial Commit

---
+++ Clock App/Clock_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clock_AppApp.swift
+//  Clock App
+//
+//  Created by me on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Clock_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
