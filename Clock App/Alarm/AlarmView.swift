82d35d31388f6517e545e812650f6ef643f1e6cd
lironasaraf <lironi06@gmail.com>
2023-08-16 17:53:53 +0300

    alarm

---
+++ Clock App/Alarm/AlarmView.swift
@@ -0,0 +1,20 @@
+//
+//  AlarmView.swift
+//  Clock App
+//
+//  Created by me on 16/08/2023.
+//
+
+import SwiftUI
+
+struct AlarmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AlarmView_Previews: PreviewProvider {
+    static var previews: some View {
+        AlarmView()
+    }
+}
